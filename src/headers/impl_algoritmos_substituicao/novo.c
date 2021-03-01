2e7b3dc5cec57e3a4d7c8650030a473b3bdd9410
Vitor <assuncao26vitor@gmail.com>
2021-03-01 19:47:36 -0300

    Novo novo

--- src/headers/impl_algoritmos_substituicao/novo.c
+++ src/headers/impl_algoritmos_substituicao/novo.c
@@ -6,12 +6,29 @@
 extern unsigned paginasOcupadas;
 extern unsigned* paginasNaMemoria;
 
+typedef struct AuxPagina {
+    unsigned tempo;
+    unsigned endereco;
+} AuxPagina;
+
+int comp (const void* p1, const void* p2) {
+    return ((AuxPagina*)p1)->tempo - ((AuxPagina*)p2)->tempo;
+}
+
 // Algoritmo novo
 // Remove as páginas aleatoriamente
 unsigned novo() {
-    
+
+    AuxPagina* aux_pagina = (AuxPagina*) malloc(paginasOcupadas * sizeof(AuxPagina));
+
+    // Preenche o vetor alocado
+    for(unsigned I=0; I<paginasOcupadas; I++) {
+        aux_pagina[I].tempo = tabelaPaginas[paginasNaMemoria[I]].tempoEntrada;
+        aux_pagina[I].endereco = paginasNaMemoria[I];
+    }
+
     // Sorteia um índice ocupado da memória e remove a página presente nesse índice
-    unsigned indice = rand() % paginasOcupadas;
-    unsigned endereco = paginasNaMemoria[indice];
-    return endereco;
+    qsort(aux_pagina, paginasOcupadas, sizeof(unsigned), comp);
+    unsigned k = rand() % paginasOcupadas;
+    return aux_pagina[k].endereco;
 }
